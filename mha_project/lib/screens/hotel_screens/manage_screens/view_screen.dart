7281ebe3dfd1ab0a387f99d60972003823853c49
Khanh <21520982@ms.uit.edu.vn>
2023-05-25 19:48:27 +0700

    added query room to viewscreen

--- mha_project/lib/screens/hotel_screens/manage_screens/view_screen.dart
+++ mha_project/lib/screens/hotel_screens/manage_screens/view_screen.dart
@@ -3,14 +3,46 @@ import 'package:flutter/material.dart';
 
 final firestoreRef = FirebaseFirestore.instance;
 
-class viewScreen extends StatelessWidget {
-  const viewScreen({super.key, required this.categoryName});
+class ViewScreen extends StatelessWidget {
+  const ViewScreen({super.key, required this.categoryName});
   final String categoryName;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('VIEW ${categoryName}'.toUpperCase())),
-      // body: StreamBuilder(stream: firestoreRef.collection('room'),),
+      appBar: AppBar(title: Text('VIEW $categoryName'.toUpperCase())),
+      body: StreamBuilder(
+        stream: firestoreRef.collection('room').snapshots(),
+        builder: (context, snapshots) {
+          if (snapshots.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (!snapshots.hasData || snapshots.data!.docs.isEmpty) {
+            return const Center(
+              child: Text('No rooms found.'),
+            );
+          }
+
+          if (snapshots.hasError) {
+            return const Center(
+              child: Text('Something went wrong...'),
+            );
+          }
+
+          final loadedData = snapshots.data!.docs;
+
+          return ListView.builder(
+              itemCount: loadedData.length,
+              itemBuilder: (context, index) {
+                final data = loadedData[index].data();
+                return Column(
+                  children: [Text(data['address']), Text(data['price'])],
+                );
+              });
+        },
+      ),
     );
   }
 }
